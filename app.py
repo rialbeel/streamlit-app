1bbc64ebdccf3a11f763f3c2f3eece31ed4bebe3
Blair Lee <rialbeel@gmail.com>
2023-06-03 21:36:16 -0700

    changes

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-st.title('Title of your application')
-st.markdown('this is **bold text** ')
-st.markdown('this is *italic text*')
+st.title('Simple Data Exploratory Analysis')
+st.markdown('To get started, please upload a .csv file of your choice using the button below')
+st.file_uploader(label = "Upload File")
 
-st.sidebar.title('title of sidebar')
-st.sidebar.markdown('markdown **text** in the *sidebar*')
+#st.sidebar.title('title of sidebar')
+#st.sidebar.markdown('markdown **text** in the *sidebar*')
 
-agree = st.checkbox('I agree')
+#agree = st.checkbox('I agree')
 
-if agree:
-    st.write('Great!')
-    st.markdown("this is markdown **text**")
+#if agree:
+#    st.write('Great!')
+#    st.markdown("this is markdown **text**")
 
-agree_sidebar = st.sidebar.checkbox('side bar checkbox')
+#agree_sidebar = st.sidebar.checkbox('side bar checkbox')
 
-if agree_sidebar:
-    st.sidebar.write('side bar checked')
+#if agree_sidebar:
+#    st.sidebar.write('side bar checked')
